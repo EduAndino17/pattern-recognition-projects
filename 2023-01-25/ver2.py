bae4163aca9e935d7a96e3c87552b033f554eb9c
EduAndino17 <eduardo.andino17@gmail.com>
2023-01-25 22:56:03 +0100

    Update ver2.py

--- 2023-01-25/ver2.py
+++ 2023-01-25/ver2.py
@@ -1,71 +1,73 @@
 import cv2
-import time
-import os
 import face_recognition
 
-def compare_to_existing_faces(face_encoding):
-    known_face_encodings = []
-    known_face_names = []
-    # Load existing faces and their names
-    for name in os.listdir("faces"):
-        if name.endswith(".jpg"):
-            image = face_recognition.load_image_file("faces/" + name)
-            encoding = face_recognition.face_encodings(image)[0]
-            known_face_encodings.append(encoding)
-            known_face_names.append(name.split(".")[0])
+# Train the model with the known faces
+known_face_encodings, known_face_names = train_model()
 
-    # Compare the detected face to the existing faces
-    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-    name = None
-    if True in matches:
-        first_match_index = matches.index(True)
-        name = known_face_names[first_match_index]
-    return name
-
-# Open the camera
+# Open the webcam
 camera = cv2.VideoCapture(0)
 
 # Set the frame width and height
 camera.set(3, 640)
 camera.set(4, 480)
 
-# Wait for the camera to warm up
-time.sleep(2)
+def train_model():
+    known_face_encodings = []
+    known_face_names = []
+    # Loop through the directory of known faces
+    for dirpath, dirnames, filenames in os.walk("faces"):
+        for filename in filenames:
+            if filename.endswith(".jpg"):
+                # Load the image
+                image = face_recognition.load_image_file(os.path.join(dirpath, filename))
+                # Get the face encoding
+                face_encoding = face_recognition.face_encodings(image)[0]
+                # Add the face encoding and name to the lists
+                known_face_encodings.append(face_encoding)
+                known_face_names.append(dirpath.split("/")[-1])
+    return known_face_encodings, known_face_names
 
-# Load the haar cascade classifier for face detection
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-# Start the camera
 while True:
+    # Get a frame from the webcam
     ret, frame = camera.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # If a face is detected
-    if len(faces) > 0:
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    # Convert the frame to RGB
+    rgb_frame = frame[:, :, ::-1]
 
-        # Get the face encoding
-        face_encoding = face_recognition.face_encodings(gray[y:y+h, x:x+w])[0]
+    # Get the face encodings of the faces in the frame
+    face_encodings = face_recognition.face_encodings(rgb_frame)
 
-        # Compare the detected face to existing faces in the directory
-        match = compare_to_existing_faces(face_encoding)
+    # Loop through the face encodings
+    for face_encoding in face_encodings:
+        # Compare the face encoding to the known face encodings
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
 
-        # If there is no match
-        if match is None:
-            #Take the picture name from user
-            picture_name = input("Enter picture name:")
-            cv2.imwrite("faces/"+picture_name +".jpg", frame)
-            print("New face added!")
-            break
-        else:
-            print("Welcome back, " + match)
+        name = "Unknown"
+        match_percentage = 0.0
 
-    cv2.imshow("Camera", frame)
+        # If there is a match
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_face_names[first_match_index]
+            match_percentage = face_recognition.face_distance([known_face_encodings[first_match_index]], face_encoding)
+            match_percentage = round(1-match_percentage[0],2)
+
+        # Draw a rectangle around the face
+        (top, right, bottom, left) = face_recognition.face_locations(rgb_frame)[0]
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with the name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name + " " + str(match_percentage*100) + "%", (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Show the frame
+    cv2.imshow("Webcam", frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the camera and close the window
+# Release the webcam and close the window
 camera.release()
 cv2.destroyAllWindows()
